11b4d00e87a1b54f3571608160baf5f9f7d5cea3
Itsalissss <80949153+Itsalissss@users.noreply.github.com>
2021-03-19 10:06:55 +0100

    Update 1. start model.py

--- 1. start model.py
+++ 1. start model.py
@@ -12,6 +12,11 @@ DepreciationFlow = CapitalStock*DepreciationCS
 InvestmentFlow = MAX(0, InvestmentExogenous)
 InvestmentExogenous = PulseSize*PulseTrain(10, 0, PulseInterval, 300)
 
+CapitalStock = InvestmentFlow-DepreciationFlow(InitialValue = 100)
+DepreciationCS = 0.1
+PulseSize = 5
+PulseInterval = 5
+
 
 def PulseTrain(current,start,steps,end):
     """Returns a 1.0 at given time intervals otherwise returns 0.0.
